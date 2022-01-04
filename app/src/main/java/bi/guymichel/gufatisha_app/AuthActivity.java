cfb31ba6b5781447b25cc44a7d31c082ce15206d
IGICOCORO <nduwimanaguymich@gmail.com>
2022-01-04 14:21:31 +0200

    created AuthActivity, activity_auth.xml

--- app/src/main/java/bi/guymichel/gufatisha_app/AuthActivity.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/AuthActivity.java
@@ -2,7 +2,11 @@ package bi.guymichel.gufatisha_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class AuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        Button btn_login = findViewById(R.id.btn_login);
+        ProgressBar login_progressBar = findViewById(R.id.login_progressBar);
+        btn_login.setOnClickListener(v -> {
+            login_progressBar.setVisibility(View.VISIBLE);
+            Intent i = new Intent(AuthActivity.this,MainActivity.class);
+            startActivity(i);
+        });
     }
 }
