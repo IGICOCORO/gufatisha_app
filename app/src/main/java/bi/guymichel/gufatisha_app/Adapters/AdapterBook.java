3894f53107bcd634eddb20bf2d81936c078c50b7
IGICOCORO <nduwimanaguymich@gmail.com>
2022-01-27 17:49:58 +0200

    created revations pages on ReservationFragment.java

--- app/src/main/java/bi/guymichel/gufatisha_app/Adapters/AdapterBook.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Adapters/AdapterBook.java
@@ -1,6 +1,5 @@
 package bi.guymichel.gufatisha_app.Adapters;
 
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,45 +9,40 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import bi.guymichel.gufatisha_app.R;
 
-
-public class AdapterHome extends RecyclerView.Adapter<AdapterHome.ViewHolder> {
+public class AdapterBook extends RecyclerView.Adapter<AdapterBook.ViewHolder>  {
     private Context context;
     private final int layoutId;
 
-
-
-    public AdapterHome(int layoutId,Context context) {
+    public AdapterBook(Context context, int layoutId) {
         this.context = context;
         this.layoutId = layoutId;
     }
 
-
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public AdapterBook.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
-        return new ViewHolder(itemView);
+        return new AdapterBook.ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
     }
+
 
     @Override
     public int getItemCount() {
         return 10;
     }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        View view;
-     ImageView card_image;
-     TextView nom_hotel;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            card_image = itemView.findViewById(R.id.card_image);
-            nom_hotel = itemView.findViewById(R.id.nom_hotel);
-            view = itemView;
+
         }
     }
 }
---
+++ app/src/main/java/bi/guymichel/gufatisha_app/Models/Reservation.java
@@ -0,0 +1,4 @@
+package bi.guymichel.gufatisha_app.Models;
+
+public class Reservation {
+}
