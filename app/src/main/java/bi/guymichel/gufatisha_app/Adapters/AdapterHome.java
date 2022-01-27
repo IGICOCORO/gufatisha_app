05b931bb804070e886edd88a25dc66f6871f2eb4
IGICOCORO <nduwimanaguymich@gmail.com>
2022-01-27 12:04:43 +0200

    well loaded recyclerview

--- app/src/main/java/bi/guymichel/gufatisha_app/Adapters/AdapterHome.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Adapters/AdapterHome.java
@@ -1,6 +1,7 @@
 package bi.guymichel.gufatisha_app.Adapters;
 
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,53 +11,48 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 
-import bi.guymichel.gufatisha_app.Fragments.HotelListFragment;
-import bi.guymichel.gufatisha_app.Host;
 import bi.guymichel.gufatisha_app.Models.Hotel;
 import bi.guymichel.gufatisha_app.R;
 
 
 public class AdapterHome extends RecyclerView.Adapter<AdapterHome.ViewHolder> {
-    HotelListFragment activity;
-    private final ArrayList<Hotel> hotels;
+    private Context context;
+    private final int layoutId;
 
-    public AdapterHome(HotelListFragment activity, ArrayList<Hotel>hotels) {
-        this.hotels =  hotels;
-        this.activity = activity;
+
+
+    public AdapterHome(int layoutId,Context context) {
+        this.context = context;
+        this.layoutId = layoutId;
     }
 
-    @NonNull
+
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_hotel_view, parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
         return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-    final Hotel hotel = hotels.get(position);
-        holder.nom_hotel.setText(hotel.nom);
-        //Picasso.get().load(Host.URL +"/"+(hotel.image)+".jpg").into(holder.card_image);
     }
 
     @Override
     public int getItemCount() {
-        return hotels.size();
+        return 10;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        View view;
      ImageView card_image;
      TextView nom_hotel;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             card_image = itemView.findViewById(R.id.card_image);
             nom_hotel = itemView.findViewById(R.id.nom_hotel);
-
-
+            view = itemView;
         }
     }
 }
--- app/src/main/java/bi/guymichel/gufatisha_app/Fragments/HotelListFragment.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Fragments/HotelListFragment.java
@@ -3,8 +3,6 @@ package bi.guymichel.gufatisha_app.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -30,18 +28,9 @@ public class HotelListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_hotel_list, container, false);
         hotellist = view.findViewById(R.id.hotellist);
-        hotellist.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false));
-        hotellist.addItemDecoration(new DividerItemDecoration(getContext(),1));
-        hotels = new ArrayList<>();
-        loadHotels();
-        adapter = new AdapterHome(this,hotels);
+        adapter = new AdapterHome(R.layout.card_hotel_view, getActivity());
         hotellist.setAdapter(adapter);
         return view;
     }
 
-    private void loadHotels() {
-        hotels.add(
-                new Hotel("Martha", null)
-        );
-    }
 }
