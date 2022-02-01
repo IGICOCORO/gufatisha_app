078c2d39cca66b51b40139c899b8aa245c6d76d7
IGICOCORO <nduwimanaguymich@gmail.com>
2022-02-01 15:56:55 +0200

    removed unnecessary activity_auth.xml

--- app/src/main/java/bi/guymichel/gufatisha_app/SplashScreenActivity.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/SplashScreenActivity.java
@@ -19,7 +19,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, AuthActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
