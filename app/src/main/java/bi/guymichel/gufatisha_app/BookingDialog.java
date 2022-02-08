73e2027756cfc64ccca54a2855b14cbebdef9367
IGICOCORO <nduwimanaguymich@gmail.com>
2022-02-08 23:29:27 +0200

    added BookingDialog on a Button (book now) in RoomActivity

---
+++ app/src/main/java/bi/guymichel/gufatisha_app/BookingDialog.java
@@ -0,0 +1,24 @@
+package bi.guymichel.gufatisha_app;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class BookingDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_register,null);
+        builder.setView(view);
+        return builder.create();
+    }
+}
