a24c2a1af1422b568cc5d60bc7dce707c2e34d6b
IGICOCORO <nduwimanaguymich@gmail.com>
2022-03-14 17:23:36 +0200

    updated gradle

--- app/src/main/java/bi/guymichel/gufatisha_app/Host.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Host.java
@@ -2,29 +2,11 @@ package bi.guymichel.gufatisha_app;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.Objects;
-
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.HttpUrl;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class Host {
-    public static String URL = "http://192.168.126.218:8000";
+    public static String URL = "http://192.168.219.218:8000";
     private static SharedPreferences sessionPreference;
 
 
