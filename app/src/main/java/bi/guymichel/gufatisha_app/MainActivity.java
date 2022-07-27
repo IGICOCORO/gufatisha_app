358def26cc82519e7ff80a3f2c5e307cb43477e9
IGICOCORO <nduwimanaguymich@gmail.com>
2022-07-27 18:17:07 +0200

    changed MainActivity and design

--- app/src/main/java/bi/guymichel/gufatisha_app/MainActivity.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/MainActivity.java
@@ -3,29 +3,36 @@ package bi.guymichel.gufatisha_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tablayout;
+    private ViewPager viewpager;
+    private PageAdapter main_fspa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_order)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        tablayout = findViewById(R.id.tablayout);
+        viewpager = findViewById(R.id.viewpager);
+        main_fspa = new PageAdapter(getSupportFragmentManager(), 1, this);
+        tablayout.setupWithViewPager(viewpager);
+        viewpager.setAdapter(main_fspa);
+        viewpager.setOffscreenPageLimit(3);
+        tablayout.getTabAt(0).setIcon(R.drawable.ic_home);
+        tablayout.getTabAt(1).setIcon(R.drawable.ic_hotel);
     }
 }
