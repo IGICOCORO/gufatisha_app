dde93d3228e592e8421c920fcd2371dac5076399
IGICOCORO <nduwimanaguymich@gmail.com>
2022-09-21 19:03:15 +0200

    updated Host

--- app/src/main/java/bi/guymichel/gufatisha_app/Host.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Host.java
@@ -1,8 +1,5 @@
 package bi.guymichel.gufatisha_app;
-
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,13 +8,12 @@ import java.util.TimeZone;
 
 public class Host {
     public static String URL = "http://192.168.164.218:8000";
-    private static SharedPreferences sessionPreference;
 
 
     public static Date getDate(String str_date) {
         Date date;
         try {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:S'Z'");
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:S'Z'");
             formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
             date = formatter.parse(str_date);
         }catch (Exception e) {
@@ -29,21 +25,12 @@ public class Host {
     public static String getStrDate(Date date) {
         String str_date;
         try {
-            SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm"); //this format changeable
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm"); //this format changeable
             dateFormatter.setTimeZone(TimeZone.getDefault());
             str_date = "Le "+ dateFormatter.format(date);
         }catch (Exception e) {
             str_date = null;
         }
         return str_date;
-    }
-
-
-    public static void logOut(Activity context){
-        sessionPreference = context.getSharedPreferences("user_session", Context.MODE_PRIVATE);
-        SharedPreferences.Editor session = sessionPreference.edit();
-        session.clear();
-        session.apply();
-        context.finish();
     }
 }
