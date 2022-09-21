5a51935ff44a2419ebf3955e427c5ce3e9fbdc72
IGICOCORO <nduwimanaguymich@gmail.com>
2022-09-21 16:21:25 +0200

    created functions for Date to String

--- app/src/main/java/bi/guymichel/gufatisha_app/Host.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Host.java
@@ -4,10 +4,39 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 
 public class Host {
-    public static String URL = "http://192.168.0.218:8000";
+    public static String URL = "http://192.168.164.218:8000";
     private static SharedPreferences sessionPreference;
+
+
+    public static Date getDate(String str_date) {
+        Date date;
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:S'Z'");
+            formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+            date = formatter.parse(str_date);
+        }catch (Exception e) {
+            e.printStackTrace();
+            date = null;
+        }
+        return date;
+    }
+    public static String getStrDate(Date date) {
+        String str_date;
+        try {
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm"); //this format changeable
+            dateFormatter.setTimeZone(TimeZone.getDefault());
+            str_date = "Le "+ dateFormatter.format(date);
+        }catch (Exception e) {
+            str_date = null;
+        }
+        return str_date;
+    }
 
 
     public static void logOut(Activity context){
