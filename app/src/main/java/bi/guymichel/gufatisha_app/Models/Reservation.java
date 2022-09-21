702886b5cf9a290f9c0fb9e05baa9fa91b8f3bd1
IGICOCORO <nduwimanaguymich@gmail.com>
2022-09-21 16:17:34 +0200

    added id in Reservation

--- app/src/main/java/bi/guymichel/gufatisha_app/Models/Reservation.java
+++ app/src/main/java/bi/guymichel/gufatisha_app/Models/Reservation.java
@@ -1,22 +1,36 @@
 package bi.guymichel.gufatisha_app.Models;
 
-public class Reservation {
-    public String date_arrivee,date_depart,client,prix_chambre, numero_chambre;
+import java.util.Date;
 
-    public Reservation( String numero_chambre,String client,String date_arrivee, String date_depart,String prix_chambre) {
-        this.date_arrivee = date_arrivee;
-        this.date_depart = date_depart;
+import bi.guymichel.gufatisha_app.Host;
+
+public class Reservation {
+    public String id,client,prix_chambre, numero_chambre;
+    public Date date_depart, date_arrivee;
+
+    public Reservation( String id,String numero_chambre,String client,String date_arrivee, String date_depart,String prix_chambre) {
+        this.id = id;
+        this.date_arrivee = Host.getDate(date_arrivee);
+        this.date_depart = Host.getDate(date_depart);
         this.client = client;
         this.numero_chambre = numero_chambre;
         this.prix_chambre = prix_chambre;
     }
 
-    public String getDate_arrivee() {
-        return date_arrivee;
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id='" + id + '\'' +
+                ", client='" + client + '\'' +
+                ", prix_chambre='" + prix_chambre + '\'' +
+                ", numero_chambre='" + numero_chambre + '\'' +
+                ", date_depart=" + date_depart +
+                ", date_arrivee=" + date_arrivee +
+                '}';
     }
 
-    public String getDate_depart() {
-        return date_depart;
+    public String getId() {
+        return id;
     }
 
     public String getClient() {
@@ -27,18 +41,15 @@ public class Reservation {
         return prix_chambre;
     }
 
-    @Override
-    public String toString() {
-        return "Reservation{" +
-                "date_arrivee='" + date_arrivee + '\'' +
-                ", date_depart='" + date_depart + '\'' +
-                ", client='" + client + '\'' +
-                ", prix_chambre='" + prix_chambre + '\'' +
-                ", numero_chambre='" + numero_chambre + '\'' +
-                '}';
-    }
-
     public String getNumero_chambre() {
         return numero_chambre;
+    }
+
+    public Date getDate_depart() {
+        return date_depart;
+    }
+
+    public Date getDate_arrivee() {
+        return date_arrivee;
     }
 }
